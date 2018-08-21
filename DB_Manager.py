1982a410d5a1fa58f244b50d7bab824d12180ba5
TPCT <eslamjackie@outlook.com>
2018-08-21 02:17:37 -0400

    Update DB_Manager.py

--- DB_Manager.py
+++ DB_Manager.py
@@ -1,4 +1,5 @@
 class DB_MANAGER:
+    from threading import Thread, Lock
     class __DB_READER_DECS:
         __DB_FILE = None
         def __get__(self, instance, owner):
@@ -14,6 +15,7 @@ class DB_MANAGER:
     class __Security:
         @staticmethod
         def Encrypt(data):
+
             return data
         def Decrypt(data):
             return data
@@ -34,9 +36,9 @@ class DB_MANAGER:
     __DB_TABLES_PATTERN = r'(?x)(?<=\<table\>).*?(?=\<\/table\>)'
     __DB_TABLE_NAME_PATTERN = r'(?x)(?<=\<table_name\=\').*?(?=\'\/>)'
     __DB_TABLE_FIELDS_PATTERN = r'(?x)(?<=\<table_fields\=\[).*?(?=\]\/>)'
-    __DB_TABLE_ROWS_PATTERN = r'(?x)(?<=\<row\s{1}).*(?=\>)'
-    __TABLE_QUERY = lambda self, table_name, table_fields: "\n<table><table_name='%s'/><table_fields=%s/></table>" % (table_name, str(table_fields))
-    __ROW_QUERY = lambda self, values: "<row %s>" % values
+    __DB_TABLE_ROWS_PATTERN = r'(?x)(?<=\<row\s).*(?=\>)'
+    __TABLE_QUERY = lambda self, table_name, table_fields: "\n\t<table>\n\t\t<table_name='%s'/>\n\t\t<table_fields=%s/>\n\t</table>\n" % (table_name, str(table_fields))
+    __ROW_QUERY = lambda self, values: "\n\t\t<row %s>" % values
     __Tables_Names, __Tables_Fields, __Tables_Rows = [], [], []
     __DB_FILE = __DB_READER_DECS()
     __DB_FILE_NAME = None
@@ -55,6 +57,7 @@ class DB_MANAGER:
                 user, passwd = self.re.findall(self.__AUTH_PATTERN, self.__DB_FILE)
                 if isinstance(passwd, list) or user != username or passwd != password:
                     raise self.__Exceptions.AuthError("Please enter Valid DB File with this authenitication values")
+                #self.__Writer_Manger = self.__ThreadingROW()
                 self.__DB_FILE_NAME = DB_File
                 self.__Logged = True
                 tables = self.re.findall(self.__DB_TABLES_PATTERN, self.__DB_FILE, flags=self.re.DOTALL)
@@ -63,6 +66,16 @@ class DB_MANAGER:
                 raise self.__Exceptions.AuthError("Please enter Valid DB File with this authenitication values") from None
         else:
             raise self.__Exceptions.AuthError("Please enter Valid DB File with this authenitication values") from None
+    global_locker = Lock()
+    def __Writer(self, file_name, data, seek=0):
+        while self.global_locker.locked():
+            continue
+        global_locker.acquire()
+        with open(file_name, 'r+') as writer:
+            writer.seek(seek)
+            writer.write(data)
+        global_locker.release()
+
     def create_table(self, Table_Name, Table_Fields=[]):
         if self.__Logged and self.__DB_FILE != None:
             if Table_Name not in self.__Tables_Names and Table_Fields:
@@ -94,7 +107,7 @@ class DB_MANAGER:
             if len(Values) == len(fields):
                 row = self.__ROW_QUERY(values)
                 self.__Tables_Rows[self.__Tables_Names.index(Table_Name)].append(values)
-                table_pos = self.re.search(r'(?x)(?<=\<table\>\<table_name=\'%s\'\/\>).*?(?=\<\/table\>)' % Table_Name, self.__DB_FILE, self.re.DOTALL)
+                table_pos = self.re.search(r'(?x)(?<=\<table_name=\'%s\'\/\>).*?(?=\n\t\<\/table\>)' % Table_Name, self.__DB_FILE, self.re.DOTALL)
                 with open(self.__DB_FILE_NAME, 'r+') as Row_Writer:
                     Row_Writer.seek(table_pos.end())
                     Row_Writer.write(row)
@@ -141,10 +154,12 @@ class DB_MANAGER:
             del self.__Tables_Rows[table_index]
             del self.__Tables_Fields[table_index]
             with open(self.__DB_FILE_NAME, 'r+') as writer:
-                table_pos = self.re.search(r'\<table\>\<table_name\=\'%s\'\/\>.*?\<\/table\>' % Table_Name, self.__DB_FILE, self.re.DOTALL)
-                writer.seek(table_pos.start())
-                writer.write('\f'*len(table_pos.group()))
-                self.__DB_FILE = self.re.sub(r'\<table\>\<table_name\=\'%s\'\/\>.*?\<\/table\>', '', self.__DB_FILE)
+                table_pos = self.re.search(r'\<table>\n\t\t\<table_name\=\'%s\'\/\>.*?\n\t\<\/table\>' % Table_Name, self.__DB_FILE, self.re.DOTALL)
+                self.Writer(self.__DB_FILE_NAME, row + self.__DB_FILE[table_pos.end():], table_pos.end())
+                writer.seek(table_pos.start()-len('\n\t'))
+                writer.truncate()
+                writer.write(self.__DB_FILE[table_pos.end():])
+                self.__DB_FILE = self.__DB_FILE[:table_pos.start()-len('\n\t')] + self.__DB_FILE[table_pos.end():]
         else:
             raise self.__Exceptions.InvalidTable("This table is either doesn't exists or You are not logged in") from None
     def delete_row(self, Table_Name, searching_by={}):
@@ -156,16 +171,17 @@ class DB_MANAGER:
             table_fields = self.__Tables_Fields[table_index]
             table_rows = self.__Tables_Rows[table_index]
             indeces = [(table_fields.index(x), str(searching_by[x])) for x in searching_by]
-            for i in range(len(table_rows)):
-                row = table_rows[i]
-                mrow = table_rows[i].split(',')
+            for row in table_rows:
+                mrow = row.split(',')
                 if all([True if mrow[index] == value else False for index, value in indeces]):
                     self.__Tables_Rows[table_index].remove(row)
-                with open(self.__DB_FILE_NAME, 'r+') as writer:
-                    row_pos = self.re.search(r'\<row %s>' % row, self.__DB_FILE)
-                    writer.seek(row_pos.start())
-                    writer.write('\f'*len(row_pos.group()))
-                    self.__DB_FILE = self.re.sub(r'(<row %s>)' % row, '', self.__DB_FILE)
+                    with open(self.__DB_FILE_NAME, 'r+') as writer:
+                        row_pos = self.re.search(r'\<row %s>' % row, self.__DB_FILE)
+                        writer.seek(row_pos.start()- len('\n\t\t'))
+                        new_data = self.__DB_FILE[row_pos.end():]
+                        writer.truncate()
+                        writer.write(new_data)
+                        self.__DB_FILE = self.__DB_FILE[:row_pos.start()-len('\n\t\t')]+self.__DB_FILE[row_pos.end():]
         else:
             raise self.__Exceptions.InvalidTable("This table is either doesn't exists or you are not logged in or you Null searcing pointer")
     def create_db(self, DB_FILE, username, password):
@@ -181,12 +197,29 @@ class DB_MANAGER:
         if DB_FILE:
             self.load(DB_FILE, username, password)
 
+class SQL_PREPROCESSOR:
+    def __init__(self, QUERY, DB_MANAGER):
+        self.__QUERY = QUERY
+        self.__DB_MANAGER = DB_MANAGER
+        self.__RESERVED_WORDS = ['SELECT', 'FROM', 'OR', 'AND', '*', 'DISTINCT', 'WHERE']
+
 if __name__ == '__main__':
+    from random import choice
+    from string import ascii_letters, punctuation
+    string_total = (ascii_letters + punctuation)*10
+    users = list(set([''.join([choice(string_total) for i in range(7)]) for x in range(20000)]))
     users_DB = DB_MANAGER()
-    users_DB.create_db('FLASK_APP', 'root', 'toor')
-    users_DB.load('FLASK_APP', 'root', 'toor')
-    users_DB.create_table('Users', ['ID', 'USERNAME', 'PASSWORD', 'FLAG'])
-    users_DB.create_row('Users', ['0', 'ISLAM', 'Th3@Professional', None])
-    users_DB.create_table('Admin', ['ID', 'Username', 'Password', 'New_FLAG'])
-    users_DB.create_row('Admin', ['0', 'Nancy', 'Th3@Professional', 'MAX'])
-    users_DB.delete_row('Users', {'ID': 0, 'PASSWORD': 'Th3@Professional'})
+    #users_DB.create_db('FLASK_APP', 'root', 'toor')
+    #users_DB.load('FLASK_APP', 'root', 'toor')
+    #users_DB.create_row('Users', ['20003', 'Ezz', 'Th3@Professional', None])
+    #users_DB.create_table('orders', ['ID', 'Name', 'Comments'])
+    #users_DB.create_row('Users', ['20001', 'Nancy', 'Th3@Professional', None])
+    #users_DB.create_table('Users', ['ID', 'USERNAME', 'PASSWORD', 'FLAG'])
+    #for i in range(len(users)):
+    #    users_DB.create_row('Users', [i, users[i], 'Th3@Professional', None])
+    #users_DB.create_table('Admin', ['ID', 'USERNAME', 'PASSWORD', 'FLAG'])
+    #for i in range(1000):
+            #users_DB.create_row('Admin', [i, users[i], 'Th3@Professional', None])
+    #orders = ['EZZ', 'SARAH', 'YARA', 'RADAWA', 'ABDELRHMAN', 'SHERIF']
+    #for i in range(6):
+    #    users_DB.create_row('orders', [i, users[i], 'TPCTROOT'])
